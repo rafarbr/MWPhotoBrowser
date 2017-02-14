13f9fe32caf094966b89be4f1a1e018dbd868167
Raphael Bernardes Rosa <raphael.rosa@ip-10-11-16-43.sa-east-1.compute.internal>
2017-02-14 19:27:27 -0200

    add overlayer on navigation bar and shared button on right

--- Pod/Classes/MWPhotoBrowserPrivate.h
+++ Pod/Classes/MWPhotoBrowserPrivate.h
@@ -15,26 +15,27 @@
 // Declare private methods of browser
 @interface MWPhotoBrowser () {
     
-	// Data
+    // Data
     NSUInteger _photoCount;
     NSMutableArray *_photos;
     NSMutableArray *_thumbPhotos;
-	NSArray *_fixedPhotosArray; // Provided via init
-	
-	// Views
-	UIScrollView *_pagingScrollView;
-	
-	// Paging & layout
-	NSMutableSet *_visiblePages, *_recycledPages;
-	NSUInteger _currentPageIndex;
+    NSArray *_fixedPhotosArray; // Provided via init
+    
+    // Views
+    UIScrollView *_pagingScrollView;
+    
+    // Paging & layout
+    NSMutableSet *_visiblePages, *_recycledPages;
+    NSUInteger _currentPageIndex;
     NSUInteger _previousPageIndex;
     CGRect _previousLayoutBounds;
-	NSUInteger _pageIndexBeforeRotation;
-	
-	// Navigation & controls
-	UIToolbar *_toolbar;
-	NSTimer *_controlVisibilityTimer;
-	UIBarButtonItem *_previousButton, *_nextButton, *_actionButton, *_doneButton;
+    NSUInteger _pageIndexBeforeRotation;
+    
+    // Navigation & controls
+    UIToolbar *_toolbar;
+    CAGradientLayer *_gradientToolBar;
+    NSTimer *_controlVisibilityTimer;
+    UIBarButtonItem *_previousButton, *_nextButton, *_actionButton, *_doneButton;
     MBProgressHUD *_progressHUD;
     
     // Grid
@@ -52,6 +53,8 @@
     UIBarButtonItem *_previousViewControllerBackButton;
     UIImage *_previousNavigationBarBackgroundImageDefault;
     UIImage *_previousNavigationBarBackgroundImageLandscapePhone;
+    NSDictionary *_previousNavBarBarTextTintColor;
+    CAGradientLayer *_gradient;
     
     // Video
     MPMoviePlayerViewController *_currentVideoPlayerViewController;
@@ -64,8 +67,8 @@
     BOOL _statusBarShouldBeHidden;
     BOOL _displayActionButton;
     BOOL _leaveStatusBarAlone;
-	BOOL _performingLayout;
-	BOOL _rotating;
+    BOOL _performingLayout;
+    BOOL _rotating;
     BOOL _viewIsActive; // active as in it's in the view heirarchy
     BOOL _didSavePreviousStateOfNavBar;
     BOOL _skipNextPagingScrollViewPositioning;
